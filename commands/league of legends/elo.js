222d1a09aca5bde8a541c52f71a222c618905718
Daniel <110414111+dKally@users.noreply.github.com>
2023-10-30 16:35:25 -0300

    Add files via upload

--- commands/league of legends/elo.js
+++ commands/league of legends/elo.js
@@ -1,54 +1,78 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageEmbed } = require('discord.js');
 const puppeteer = require('puppeteer');
-const play = require('../../../discord-music-bot-main (cópia)/slash/play');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('elo')
-        .setDescription('Insira o nome do player que quer ver o elo.')
+        .setDescription('Insira o nome do jogador para ver o elo.')
         .addStringOption(option =>
             option
                 .setName('nome')
-                .setDescription('Nome do player')
+                .setDescription('Nome do jogador')
                 .setRequired(true)
         ),
     async execute(interaction) {
         try {
-            let target = interaction.options.getString('nome')
+            const target = interaction.options.getString('nome');
             const browser = await puppeteer.launch({ headless: "new" });
             const page = await browser.newPage();
             await page.goto(`https://blitz.gg/lol/profile/br1/${target}`);
-            console.log(`https://blitz.gg/lol/profile/br1/${target}`)
-            let player = target
+            await new Promise(resolve => setTimeout(resolve, 500));
+
+            let player = target;
 
             const topLineText = await page.evaluate(() => {
                 const rankElement = document.querySelector('.name');
                 const pdlElement = document.querySelector('.points');
-                const winRateElement = document.querySelector('.bottom-line');
 
-
-                if (rankElement && pdlElement && winRateElement) {
+                if (rankElement && pdlElement) {
                     const rank = rankElement.textContent.trim();
                     const pdl = pdlElement.textContent.trim();
-                    const winRate = winRateElement.textContent.trim();
-                    return { rank, pdl, winRate };
+                    return { rank, pdl };
+                } else {
+                    return null;
+                }
+            });
+
+            const winRates = await page.evaluate(() => {
+                const winRateElement = document.querySelector('.bottom-line');
+
+                if (winRateElement) {
+                    const spans = winRateElement.querySelectorAll('span');
+                    const winRateTexts = [];
+
+                    spans.forEach(span => {
+                        winRateTexts.push(span.textContent.trim());
+                    });
+
+                    return winRateTexts;
                 } else {
                     return null;
                 }
             });
 
             await browser.close();
-            target = ''
 
             if (topLineText) {
-                const response = `**${player}**
-Rank atual: ${topLineText.rank} ${topLineText.pdl}
-${topLineText.winRate}`;
-                await interaction.reply(response);
+                const embed = {
+                    title:`Elo de ${player}`,
+                    fields:[
+                        {
+                            name:'Rank Atual:',
+                            value: topLineText.rank + topLineText.pdl
+                        },{
+                            name:'WinRate',
+                            value: winRates[0] + ' ' + winRates[1]
+                        }
+                    ]
+                }
+
+                await interaction.reply({
+                    embeds: [embed],
+                });
             } else {
                 await interaction.reply('Rank não encontrado...');
             }
-            player = ''
         } catch (error) {
             console.error(error);
             await interaction.reply('Ocorreu um erro ao buscar o rank do jogador.');
