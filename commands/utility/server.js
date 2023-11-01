5aaf75cc0df52aa00eb2f4f2c23d07f828f2d7d4
Daniel <110414111+dKally@users.noreply.github.com>
2023-11-01 18:50:54 -0300

    Add files via upload

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -1,10 +1,35 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
 		.setDescription('Display info about this server.'),
 	async execute(interaction) {
-		return interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+		const { guild } = interaction;
+
+		const embed = {
+			title: guild.name,
+			fields: [
+				{
+					name: 'ID do Servidor:',
+					value: guild.id,
+				},
+				{
+					name: 'Data de Criação:',
+					value: guild.createdAt.toDateString(),
+				},
+				{
+					name: 'Membros Totais:',
+					value: guild.memberCount,
+				},
+			],
+			thumbnail:{
+						url: guild.iconURL({ dynamic: true, format: 'png' })
+					}
+			,
+			color: 0x9900ff,
+		};
+
+		return interaction.reply({embeds:[embed]});
 	},
 };
